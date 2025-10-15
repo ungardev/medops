f253e35e47ea991b4c3c7e02febb0a825347056f
ungardev <ungardev@outlook.com>
2025-10-14 20:55:02 -0400

    Config: incluir core/static en STATICFILES_DIRS

--- medops/settings.py
+++ medops/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "medops" / "dist" / "assets",
+    BASE_DIR / "core" / "static"
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
