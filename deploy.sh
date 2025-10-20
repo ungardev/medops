28452416fc51aaa092eb70057f8588e3774a1e04
ungardev <ungardev@outlook.com>
2025-10-20 16:50:24 -0400

    chore: actualizar deploy.sh para usar medops.service

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,17 @@ log "🚀 Iniciando despliegue de MedOps..."
 log "Actualizando repositorio..."
 git pull origin main | tee -a "$LOGFILE"
 
-# 2. Frontend
+# 2. Instalar dependencias backend (opcional pero recomendado)
+log "Instalando dependencias backend..."
+pipenv install --deploy --ignore-pipfile >> "$LOGFILE" 2>&1
+
+# 3. Frontend
 log "Construyendo frontend..."
 cd frontend/medops
 npm install >> "$LOGFILE" 2>&1
 npm run build >> "$LOGFILE" 2>&1
 
-# 3. Backend
+# 4. Backend
 cd /srv/medops
 log "Recolectando estáticos..."
 pipenv run python manage.py collectstatic --noinput >> "$LOGFILE" 2>&1
@@ -29,9 +33,9 @@ pipenv run python manage.py collectstatic --noinput >> "$LOGFILE" 2>&1
 log "Aplicando migraciones..."
 pipenv run python manage.py migrate >> "$LOGFILE" 2>&1
 
-# 4. Reiniciar servicios
-log "Reiniciando Gunicorn..."
-sudo systemctl restart gunicorn
+# 5. Reiniciar servicios
+log "Reiniciando Gunicorn (medops.service)..."
+sudo systemctl restart medops
 
 log "Recargando Nginx..."
 sudo systemctl reload nginx
