c52c82c8a1c634a5c6fb632f758089144cf2b1f2
ungardev <ungardev@outlook.com>
2025-11-17 22:28:22 -0400

    fix api_views.py

--- core/api_views.py
+++ core/api_views.py
@@ -2889,7 +2889,7 @@ def generate_referral_pdf(request, pk):
     responses={201: ChargeOrderSerializer},
     description="Genera una orden de cobro en PDF y la registra como documento clínico."
 )
-@api_view(["GET", "POST"])   # 👈 ahora acepta GET y POST
+@api_view(["GET", "POST"])   # 👈 acepta GET y POST
 @permission_classes([IsAuthenticated])
 def generate_chargeorder_pdf(request, pk):
     charge_order = get_object_or_404(ChargeOrder, pk=pk)
@@ -2990,7 +2990,8 @@ def generate_chargeorder_pdf(request, pk):
         notify=True
     )
 
-    return Response(ChargeOrderSerializer(charge_order).data, status=status.HTTP_201_CREATED)
+    # ✅ Devolver el PDF binario
+    return FileResponse(open(full_path, "rb"), content_type="application/pdf", as_attachment=True, filename=filename)
 
 
 @extend_schema(
