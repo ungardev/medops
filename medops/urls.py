111992d3fb3dc7b6daeb5a9dc92a6f950d4c4770
ungardev <ungardev@outlook.com>
2026-01-08 03:25:23 -0400

    fix urls.py

--- medops/urls.py
+++ medops/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.api_urls")),
+
+    # 👇 Endpoint de login basado en DRF Token
+    path("api/auth/login/", obtain_auth_token, name="api_token_auth"),
 ]
 
 if settings.DEBUG:
