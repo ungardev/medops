bfcf77c5868f33b15dca317b7876130280a8c32a
ungardev <ungardev@outlook.com>
2026-01-19 12:48:19 -0400

    fix api_views.py add import settings

--- core/api_views.py
+++ core/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status, views
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.conf import settings
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
