3d8cc5e0fc118e48773bb72f87ae81dcea10bd11
ungardev <ungardev@outlook.com>
2026-05-26 20:31:34 -0400

    fix(backend): serve media files in production with serve() view
    - Add re_path for media URLs when DEBUG=False
    - Import django.views.static.serve for production media serving
    - Prepare infrastructure for Railway persistent volumes

--- medops/urls.py
+++ medops/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -34,3 +35,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
